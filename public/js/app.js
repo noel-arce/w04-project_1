e74ee7907a759407663c01a0de8f311bbeca09e4
noel-arce <n.arcellana.00@gmail.com>
2015-10-25 14:25:39 -0700

    client-side delete, title save, and modal prepend

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 // CLIENT-SIDE JAVASCRIPT
 // On page load
 $(document).ready(function(){
-  console.log('Ready to GO!');
+  console.log('JS ready to GO!');
 
   //set class='san francisco' to list
   $('#west-coast').on('click', function (e) {
@@ -12,7 +12,7 @@ $(document).ready(function(){
   //set class='chicago' to list
   $('#mid-west').on('click', function (e) {
     e.preventDefault();
-    console.log('city set to Chicgao');
+    console.log('city set to Chicago');
   });
 
   //set class='new york' to list
@@ -22,28 +22,40 @@ $(document).ready(function(){
   });
 
   //click-handler for post-form to make list
-  $('.trip-idea').on('submit', function (e) {
+  $('.postIdea').on('submit', function (e) {
   	e.preventDefault();
   	console.log('idea was added to list');
 
   	if ($('#post').val().trim().length > 0) {
   		var ideaContent = $('#post').val();
   		var newIdea = '<li class="list-group-item">' + ideaContent + '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>' + '</li>';
-  		$('#itinerary').prepend(newIdea);
+  		$('.itinerary').prepend(newIdea);
   		$('#post').val('');
   	}
+  });
+
+  //click handler for delete list-item
+  $('.itinerary').on('click', '.glyphicon-remove', function (e) {
+    e.preventDefault();
+    var deletedPost = $(this).closest('li');
+
+    $(deletedPost).remove();
+    console.log('item removed');
   });
 
   //click handler for save button - save to database and render to Past Plans
   $('#save').on('click', function (e) {
     e.preventDefault();
-    console.log('itinerary saved to database');
-  });
+    //console.log('itinerary saved to database');
 
-  //click thumbnail to get it to zoom in
-  $('.thumbnail').on('click', function (e) {
-    e.preventDefault();
-    console.log('get the thumbnail to enlarge!!!');
+    if ($('#title').val().trim().length > 0) {
+      var title = $('#title').val();
+      var newModal = '<a type="button" class="btn btn-lg" data-toggle="modal" data-target="#myModal">' + title +
+         '</a>';
+      //console.log(title);
+
+      $('#SF').prepend(newModal);
+    }
   });
 
 });
